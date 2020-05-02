4c782e313e9e49774b8935dc1d714583502f7532
Andrei <a.e.itiligator@gmail.com>
2020-05-02 12:43:24 +0700

    Очищен списко точек доступа к API

--- mprbackend/api/urls.py
+++ mprbackend/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products', views.productsjson, name='productsjson'),
-    path('clients', views.clientsjson, name='clientsjson'),
-    path('orderlist', views.orderlist, name='orderlist'),
-    path('getorder/<int:pk>', views.getorder, name='getorder'),
-    path('putorder', views.putorder, name='putorder'),
+    # path('products', views.productsjson, name='productsjson'),
+    # path('clients', views.clientsjson, name='clientsjson'),
+    # path('orderlist', views.orderlist, name='orderlist'),
+    # path('getorder/<int:pk>', views.getorder, name='getorder'),
+    # path('putorder', views.putorder, name='putorder'),
 ]
