804f680e240d08e34149495e56833c4e091e03db
Andrey Itiligator <a.e.itiligator@gmail.com>
2020-05-18 17:54:13 +0700

    Написана заготовка сериализатора ответа чеклиста

--- mprbackend/api/serializers.py
+++ mprbackend/api/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from .models import ChecklistQuestion
+from .models import ChecklistQuestion, ChecklistAnswer
 
 
 class ChecklistQuestionSerializer(serializers.ModelSerializer):
     clientType = serializers.CharField(source='client_type')
+
     class Meta:
         model = ChecklistQuestion
         fields = ['UUID', 'clientType', 'text', 'active', 'section']
+
+
+class ChecklistAnswerSerializer(serializers.ModelSerializer):
+    visitUUID = serializers.SlugRelatedField(source='visit', slug_field='UUID', read_only=True)
+    questionUUID = serializers.PrimaryKeyRelatedField(source='question', read_only=True)
+
+    class Meta:
+        model = ChecklistAnswer
+        fields = ['visitUUID', 'questionUUID', 'answer1', 'answer2', 'UUID']
